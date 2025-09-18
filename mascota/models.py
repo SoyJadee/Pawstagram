c0d5f28a3276f3a38b5fbfd9757fafcbabff72b5
Jay <jademoya0101@gmail.com>
2025-09-18 19:09:13 -0400

    arreglando problemas de modelos

--- mascota/models.py
+++ mascota/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+
+
 class Pet(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="ID de mascota")
-    owner = models.ForeignKey('users.User', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=20,verbose_name="Nombre")
-    description = models.TextField(null=True, blank=True,verbose_name="Descripción")
-    profile_photo_url = models.TextField(null=True, blank=True,verbose_name="URL de foto de perfil")
-    is_available_for_adoption = models.BooleanField(default=False,verbose_name="Disponible para adopción")
-    created_at = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creación")
+    from django.conf import settings
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=20, verbose_name="Nombre")
+    description = models.TextField(
+        null=True, blank=True, verbose_name="Descripción")
+    profile_photo_url = models.TextField(
+        null=True, blank=True, verbose_name="URL de foto de perfil")
+    is_available_for_adoption = models.BooleanField(
+        default=False, verbose_name="Disponible para adopción")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creación")
