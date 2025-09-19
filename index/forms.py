5c13c2c8ec8517f22cecd03b6f8b9a9465bab33e
andreamejia <189182868+mejia1501@users.noreply.github.com>
2025-09-18 23:42:10 -0400

    feat: Add base HTML template with responsive design and footer
    - Created a new base.html template with Tailwind CSS for styling.
    - Implemented a responsive navigation bar with search functionality.
    - Added a footer section with company information, quick links, and social media icons.
    - Included animations for counters and paw prints in the footer.
    - Added JavaScript functions for mobile menu and search toggling.
    
    chore: Initialize URL configuration
    
    - Created an empty urls.py file for future URL routing.
    
    feat: Add pet model fields in migration
    
    - Added new fields to the Pet model: age, breed, creator, gender, ubication, and vacunas.
    - Updated the owner field to allow null values.
    
    feat: Create initial migration for VeterinaryClinic model
    
    - Created the VeterinaryClinic model with fields for name, address, phone, email, consult price, services, description, photo URL, opening and closing hours, specialties, valuation, and comments.

---
+++ index/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from index.models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content', 'post']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'w-full p-2 border border-gray-300 rounded-md',
+                'rows': 2,
+                'placeholder': 'Escribe un comentario...',
+                'label': 'Comentario',
+            }),
+        }
--- salud/models.py
+++ salud/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class VeterinaryClinic(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    consultPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    services = models.TextField(max_length=200, null=True, blank=True, verbose_name="Servicios")
+    description = models.TextField(max_length=200, null=True, blank=True, verbose_name="Descripción")
+    photo_url = models.URLField(null=True, blank=True, verbose_name="URL de foto")
+    horarioStart = models.TimeField(null=True, blank=True, verbose_name="Horario de apertura")
+    horarioEnd = models.TimeField(null=True, blank=True, verbose_name="Horario de cierre")
+    specialties = models.TextField(max_length=200, null=True, blank=True, verbose_name="Especialidades")
+    valoration = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True, verbose_name="Valoración")
+    comments = models.TextField(max_length=1000, null=True, blank=True, verbose_name="Comentarios")
+
+    def __str__(self):
+        return self.name
