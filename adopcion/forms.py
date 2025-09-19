f3f741ae86cbc27631273b31c4bf3b013fb07e5f
andreamejia <189182868+mejia1501@users.noreply.github.com>
2025-09-19 10:51:18 -0400

    feat: Refactor adoption and pet models, update forms and views for new fields, and enhance user authentication flow

--- adopcion/forms.py
+++ adopcion/forms.py
@@ -4,8 +4,27 @@ from adopcion.models import Adoption
 class AdoptionForm(forms.ModelForm):
     class Meta:
         model = Adoption
-        fields = [ 'message']
+        fields = [ 'adopterName', 'adopterEmail', 'adopterPhone', 'message']  # 'pet' y 'responsable' se asignan en la vista
         widgets = {
+            
+            'adopterName': forms.TextInput(attrs={
+                'class': 'w-full p-3 border border-gray-200 rounded-xl focus:outline-none focus:border-mint',
+                'maxlength': 40,
+                'placeholder': 'Tu nombre completo',
+                'label': 'Nombre del adoptante',
+            }),
+            'adopterEmail': forms.EmailInput(attrs={
+                'class': 'w-full p-3 border border-gray-200 rounded-xl focus:outline-none focus:border-mint',
+                'maxlength': 100,
+                'placeholder': 'Tu correo electrónico',
+                'label': 'Email del adoptante',
+            }),
+            'adopterPhone': forms.TextInput(attrs={
+                'class': 'w-full p-3 border border-gray-200 rounded-xl focus:outline-none focus:border-mint',
+                'maxlength': 20,
+                'placeholder': 'Tu número de teléfono',
+                'label': 'Teléfono del adoptante',
+            }),
             'message': forms.Textarea(attrs={
                 'class': 'w-full p-3 border border-gray-200 rounded-xl focus:outline-none focus:border-mint h-24 resize-none',
                 'rows': 4,
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('reset_password_sent/', PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_form.html', success_url='iniciar_sesion/reset_password_complete/'), name='password_reset_confirm'),
     path('reset_password_complete/', PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
+    path('perfil/', views.perfil_view, name='perfil'),
 ]
