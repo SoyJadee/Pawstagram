b1f9f8dbef943079d6e34a22fc373dc582019a34
andreamejia <189182868+mejia1501@users.noreply.github.com>
2025-09-20 12:14:27 -0400

    Añade modelo de Notificación y vista de búsqueda; actualiza plantilla de resultados para mostrar mascotas encontradas.

--- index/urls.py
+++ index/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.principal, name='principal'),
     path('like/', views.like_post, name='like_post'),
+    path('search/', views.search, name='search'),
 ]
