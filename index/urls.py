462cfae57823bda4ac847b94e2b0df3022c37954
Jay <jademoya0101@gmail.com>
2025-09-20 01:19:17 -0400

    ya funcionan los likes y se actualizan realtime

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-	path('', views.principal, name='principal'),
+    path('', views.principal, name='principal'),
+    path('like/', views.like_post, name='like_post'),
 ]
