8f9903fc700912771cfc251665b3896f881531b6
Sebastian <sebastiansanchezar3@gmail.com>
2025-09-20 20:53:49 -0400

    comentarios

--- salud/urls.py
+++ salud/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.servicios_salud, name='servicios_salud'),
+	path('obtener-comentarios-salud/', views.obtener_comentarios_salud, name='obtener_comentarios_salud'),
+	path('guardar-comentario-salud/', views.guardar_comentario_salud, name='guardar_comentario_salud'),
 ]
