dba0d07d18e8f6e7fd3f003003f79e1f356955b6
Sebastian <sebastiansanchezar3@gmail.com>
2025-09-22 20:29:20 -0400

    Arreglo de mapa

--- salud/urls.py
+++ salud/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('', views.servicios_salud, name='servicios_salud'),
 	path('obtener-comentarios-salud/', views.obtener_comentarios_salud, name='obtener_comentarios_salud'),
 	path('guardar-comentario-salud/', views.guardar_comentario_salud, name='guardar_comentario_salud'),
+    path('ruta-direcciones/', views.obtener_ruta_openrouteservice, name='ruta-direcciones'),
 ]
